628c34eb4264006e0898afbb39aebdf89a3efcca
brandonmoua24 <brandonmoua123@gmail.com>
2023-12-20 22:40:33 -0500

    Played around with set up

---
+++ jub_presentation/src/components/HomePage/homepage.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './homepage.css';
+
+function homepage () {
+    return(
+        <div className='homepage-container'>
+            <div>Sun </div>
+            <div>Mercury </div>
+            <div>Venus </div>
+            <div>Earth </div>
+            <div>Mars </div>
+            <div>Jupiter </div>
+            <div>Saturn </div>
+            <div>Uranus </div>
+            <div>Neptune</div>
+        </div>
+    )
+}
+
+export default homepage;
